cbeb2c12c4bdb7cb0ea8b3a4cb290e39975aff0b
Fauzanyusuf <44010289+Fauzanyusuf@users.noreply.github.com>
2025-12-15 11:28:35 +0700

    implement a hoc for managing window states and add the first window - terminal

---
+++ src/components/WindowControls.jsx
@@ -0,0 +1,16 @@
+﻿import React from "react";
+import useWindowStore from "@store/window.js";
+
+function WindowControls({target}) {
+  const {closeWindow} = useWindowStore();
+  
+  return (
+    <div id="window-controls">
+      <div className="close" onClick={() => closeWindow(target)}/>
+      <div className="disable"/>
+      <div className="disable"/>
+    </div>
+  );
+}
+
+export default WindowControls;
